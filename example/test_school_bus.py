08e1b6b6a4b26f5d13591eb8032c7e9d0ec49554
Michael Gilliland <mjg.py3@gmail.com>
2014-11-23 20:35:58 -0500

    Added a python example

---
+++ example/test_school_bus.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from school_bus import *
+import unittest
+
+class TestSchoolBus(unittest.TestCase):
+    def test_it_fails(self):
+        assert False
+
+if __name__ == '__main__':
+    unittest.main()
